c4d7e0611ff0144139b38d55d8f7b536628e6e69
leanciappa <ciapparelli.leandro@gmail.com>
2023-08-31 00:16:09 -0300

    V2.0

---
+++ app.js
@@ -0,0 +1,17 @@
+class Carrito{
+    constructor(){
+        this.listaCarrito = []
+    }
+
+    agregar (producto){
+        this.listaProductos.push(productos)
+    }
+}
+
+class ProductController{
+    constructor(){
+        this.listaProductos = []
+    }
+}
+
+const controladorProductos = new ProductController ()

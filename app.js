aa94aaf290eb39eaa5c9c6823dc91d2fc0748e4f
leanciappa <ciapparelli.leandro@gmail.com>
2023-09-12 21:36:09 -0300

    V2.0

--- app.js
+++ app.js
@@ -1,21 +1,65 @@
-class Cart{
-    constructor(id, category, price, description ){
-        this.id = id
-        this.category = category
-        this.price = price
-        this.description = description
-        
-    }
-
-    agregar (products){
-        this.listProduct.push(products)
+class Producto{
+    constructor(id, nombre, precio){
+        this.id = id,
+        this.nombre = nombre,
+        this.precio = precio,
+        this.cantidad = 1
     }
 }
-
-class ProductController{
+    
+    class ControladorProducto{
     constructor(){
-        this.listProduct = []
+        this.listaProductos= []
+    }
+    agregar (producto){
+        this.listaProductos.push(producto)
+    }
+    mostrar(){
+        let descripcion= ""
+        this.listaProductos.forEach(producto =>{
+            
+            descripcion += "Id:"+producto.id+" Nombre:"+producto.nombre+" Precio:"+producto.precio+" Cantidad:"+producto.cantidad+"\n"
+        })
+    return descripcion;
     }
 }
 
-const productsController = new ProductController ()
+class Carrito {
+    constructor(){
+
+        this.listaCarrito = [],
+        this.total= 0
+    }
+}
+
+
+alert("Ingresar el listado de productos del ecommerce")
+
+const controladorProductos = new ControladorProducto()
+
+
+//Generación de listado de productos 
+let rta= " "
+
+do{
+    let id = prompt ("Ingrese el id del producto: ")
+    let nombre = prompt ("Ingrese el nombre del producto: ")
+    let precio = Number(prompt ("Ingrese el precio del producto: "))
+    let cantidad = Number(prompt ("Ingrese la cantidad: "))
+    
+    let producto1 = new Producto (id, nombre, precio, cantidad)
+    controladorProductos.agregar (producto1)
+    alert(controladorProductos.mostrar())
+        
+    rta = prompt ("Si Desea salir de la carga de productos, escriba 'ESC':")
+} while (rta != 'ESC')
+
+
+
+// Compra de productos
+
+alert("Elija el productos a comprar:"+"\n"+controladorProductos.mostrar())
+
+
+
+
